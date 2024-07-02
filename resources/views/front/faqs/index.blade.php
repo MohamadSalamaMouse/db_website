4d0fb3cc280a9029240f0d2e452a4f896e98486f
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-07-02 22:02:42 +0300

    faq

---
+++ resources/views/front/faqs/index.blade.php
@@ -0,0 +1,21 @@
+@php
+    $languages = \App\Facades\UtilityFacades::languages();
+@endphp
+
+@extends('layouts.front.app')
+@section('title', __('FAQs'))
+
+@section('auth-topbar')
+    <li class="language-btn">
+        <select class="btn btn-primary me-2 nice-select"
+            onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);"
+            id="language">
+            @foreach ($languages as $language)
+                <option class="" @if ($lang == $language) selected @endif
+                    value="{{ route('change.lang', $language) }}">{{ Str::upper($language) }}
+                </option>
+            @endforeach
+        </select>
+    </li>
+
+@endsection

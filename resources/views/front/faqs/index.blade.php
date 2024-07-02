9b1483cbb613939506a1e9254889c67a5a17bd52
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-07-02 22:13:02 +0300

    Update index.blade.php

--- resources/views/front/faqs/index.blade.php
+++ resources/views/front/faqs/index.blade.php
@@ -5,17 +5,45 @@
 @extends('layouts.front.app')
 @section('title', __('FAQs'))
 
-@section('auth-topbar')
-    <li class="language-btn">
-        <select class="btn btn-primary me-2 nice-select"
-            onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);"
-            id="language">
-            @foreach ($languages as $language)
-                <option class="" @if ($lang == $language) selected @endif
-                    value="{{ route('change.lang', $language) }}">{{ Str::upper($language) }}
-                </option>
-            @endforeach
-        </select>
-    </li>
+<section id="faq" class="section border-0 m-0">
+    <div class="container">
+        <div class="row justify-content-center pt-3">
+            <div class="col-lg-10 text-center">
+                <h3 class="text-9 line-height-3 text-transform-none font-weight-semibold mb-4 appear-animation animated fadeInUpShorter appear-animation-visible"
+                    data-appear-animation="fadeInUpShorter" data-appear-animation-delay="500"
+                    style="animation-delay: 500ms;">
+                    {{ @Utility::getsettings('faq_name') }}
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 col-lg-12  mb-5 mb-md-0">
+                <div class="accordion custom-accordion-style-1 appear-animation animated fadeInUpShorter appear-animation-visible"
+                    data-appear-animation="fadeInUpShorter" data-appear-animation-delay="250" id="accordion1"
+                    style="animation-delay: 250ms;">
+                    @foreach ($faqs as $key => $record)
+                        <div class="card card-default">
+                            <div class="card-header" id="collapse{{ $key }}Heading{{ $key }}">
+                                <h4 class="card-title m-0">
+                                    <a class="accordion-toggle text-color-dark font-weight-bold collapsed"
+                                        data-bs-toggle="collapse" data-bs-target="#collapse{{ $key }}"
+                                        aria-expanded="false" aria-controls="collapse{{ $key }}">
+                                        {{ $record['questions'] }}
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapse{{ $key }}" class="collapse"
+                                aria-labelledby="collapse{{ $key }}Heading{{ $key }}"
+                                data-bs-parent="#accordion{{ $key }}" style="">
+                                <div class="card-body">
+                                    <p class="mb-0">{{ $record['answer'] }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
 
-@endsection
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
